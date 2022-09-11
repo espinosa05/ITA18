404a64b54aeed9379962514d5b711a5a390e2a3b
gaddin <80589591+gaddin@users.noreply.github.com>
2022-09-11 20:04:57 +0200

    Update Stack_beispiel.asm

--- Architektur/Beispiele/Stack_beispiel.asm
+++ Architektur/Beispiele/Stack_beispiel.asm
@@ -1,3 +1,12 @@
+; /* C Programm */
+; int main() {
+;   int a, b;
+;   a = 33;
+;   b = 22;
+;   return STACK_FUNCTION_PARAMETER_ADD(a, b); 
+;}
+;
+
 %DEFINE SYS_EXIT        1
 %DEFINE EXIT_SUCCESS    0
 
@@ -10,23 +19,27 @@ _START:
 
     CALL MAIN
 
-    MOV     EBX, EAX
-    MOV     EAX, SYS_EXIT
-    INT     0x80
+    MOV     EBX, EAX        ; Return Code
+    MOV     EAX, SYS_EXIT   ; Kernel Funktion   
+    INT     0x80            ; Linux Kernel Aufruf
 
 
 MAIN:
 
-    PUSH    EBP
-    MOV     EBP, ESP
-    SUB     ESP, 8
+    PUSH    EBP             
+    MOV     EBP, ESP        ; Neuer stack frame
+    SUB     ESP, 8          ; 8 Bytes reservieren
 
 
-    MOV     DWORD [EBP - 4], 33
-    MOV     DWORD [EBP - 8], 22
+    MOV     DWORD [EBP - 4], 33 ; die ersten 4 bytes mit 32 beschreiben
+    MOV     DWORD [EBP - 8], 22 ; die zweiten 4 bytes mit 22 beschreiben
 
-    PUSH    DWORD [EBP - 4]
-    PUSH    DWORD [EBP - 8]
+    ; Parameter übergabe (CDECL-Konvention)
+    ; Equivalenter C-code:
+    ; STACK_FUNCTION_PARAMETER_ADD(a, b);
+    
+    PUSH    DWORD [EBP - 4] 
+    PUSH    DWORD [EBP - 8] 
     CALL    STACK_FUNCTION_PARAMETER_ADD
 
     POP     EBP
