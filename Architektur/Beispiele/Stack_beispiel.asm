5e9a8b42facf54879a0a1f2ac3f8d048f77912f2
gaddin <80589591+gaddin@users.noreply.github.com>
2022-09-11 19:58:27 +0200

    Update Stack_beispiel.asm

--- Architektur/Beispiele/Stack_beispiel.asm
+++ Architektur/Beispiele/Stack_beispiel.asm
@@ -1,4 +1,3 @@
-
 %DEFINE SYS_EXIT        1
 %DEFINE EXIT_SUCCESS    0
 
@@ -8,43 +7,40 @@ GLOBAL _START
 
 
 _START:
-    
+
     CALL MAIN
 
     MOV     EBX, EAX
     MOV     EAX, SYS_EXIT
-    INT     0x80                
+    INT     0x80
 
 
 MAIN:
-    
+
     PUSH    EBP
     MOV     EBP, ESP
-    SUB     ESP, 8              
-    
+    SUB     ESP, 8
+
 
     MOV     DWORD [EBP - 4], 33
     MOV     DWORD [EBP - 8], 22
-    
+
     PUSH    DWORD [EBP - 4]
     PUSH    DWORD [EBP - 8]
     CALL    STACK_FUNCTION_PARAMETER_ADD
-   
+
     POP     EBP
     RET
 
 STACK_FUNCTION_PARAMETER_ADD:
-    PUSH    EBP                 
-    MOV     EBP, ESP            
-    
+    PUSH    EBP
+    MOV     ESP, EBP
+
     MOV     EDI, [EBP - 4]
     MOV     ESI, [EBP - 8]
 
     ADD     EDI, ESI
     MOV     EAX, EDI
-    
-
-    MOV     EAX, [EBP - 4]      
 
     POP     EBP
-    RET     
+    RET
